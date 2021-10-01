0adba7bdb8d572759c0cd9fcc0c3d29ff9744bdb
Guilhermino Gomes <guilhermino@hotmail.com.br>
2021-10-01 18:15:51 -0300

    place_window_in_program

--- interface.py
+++ interface.py
@@ -1,22 +1,28 @@
 import tkinter as tk
 
 app = tk.Tk()
-app.geometry('500x400')
-app.resizable(0, 0)
+app.title("Place() method")
+app.geometry('200x200')
+app.resizable(1, 1)
 
-backFrame = tk.Frame(master=app, width=500, height=400, bg = 'yellow').pack()
-button1 = tk.Button(master=backFrame, text='Button 1', bg='blue', fg='red').pack()
+backFrame = tk.Frame(master=app, width=200, height=200, bg = 'yellow').pack()
 
-button2 = tk.Button(master=backFrame, text='Button 2', bg='blue', fg='green').pack()
-button3 = tk.Label(master=backFrame, text='Button 3', bg='red',fg='blue').pack()
+
+button1 = tk.Button(master=backFrame, text='Login', bg='blue', fg='red', ).pack()
+button2 = tk.Button(master=backFrame, text='teste', bg='blue', fg='green').pack()
+#button3 = tk.Label(master=backFrame, text='Button 3', bg='red',fg='blue').pack()
 
 
 app.mainloop()
 
 
-
+"""
+    Estudar formas de criar uma caixa de diálogo.
+    para inserção de dados
+# backFrame.pack()
+# backFrame.propagate(0)
 
 
 # from tkinter import ttk
 
-# root_window = ttk()
+# root_window = ttk() """
