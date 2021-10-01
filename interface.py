ea4c069ef353d92f5aaa1a1716fcc202f03155f0
Guilhermino Gomes <guilhermino@hotmail.com.br>
2021-10-01 17:57:35 -0300

    ...

--- interface.py
+++ interface.py
@@ -1,3 +1,22 @@
-from tkinter impor
+import tkinter as tk
 
-root_window = Tk()
+app = tk.Tk()
+app.geometry('500x400')
+app.resizable(0, 0)
+
+backFrame = tk.Frame(master=app, width=500, height=400, bg = 'yellow').pack()
+button1 = tk.Button(master=backFrame, text='Button 1', bg='blue', fg='red').pack()
+
+button2 = tk.Button(master=backFrame, text='Button 2', bg='blue', fg='green').pack()
+button3 = tk.Label(master=backFrame, text='Button 3', bg='red',fg='blue').pack()
+
+
+app.mainloop()
+
+
+
+
+
+# from tkinter import ttk
+
+# root_window = ttk()
