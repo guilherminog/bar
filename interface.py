de2a4d10ca7b5a0f3ead50889e3ad573846234f0
Guilhermino Gomes <guilhermino@hotmail.com.br>
2021-10-01 19:00:19 -0300

    construção da inserção de dados e das labels e buttons no Tkinter

--- interface.py
+++ interface.py
@@ -1,8 +1,30 @@
+""" Essa inserção de dados será feita pela UI para sqlite
+    não esquecer de estudar as bibliotecas de dados para
+    guardar e acessar, tudo isso pelo 'tkinter'.
+    Data de inicio 30/09/2021"""
+
+
+
+from tkinter import *
 import tkinter as tk
+from insert_data import inf_init, inf_contato
+
+nome, idade = inf_init()
+tel, email, endereco = inf_contato()
+
+
+def _inf_contato():
+    print ("Nome:%s" % nome.get(inf_init(nome)))
+    print ("Idade:%s" % idade.get(inf_init(idade)))
+    print ("Email:%s" % email.get(inf_init(email)))
+    print ("Telefone:%s" % tel.get(inf_init(tel)))
+    print ("Edereço:%s" % endereco.get(inf_init(endereco)))
+
+
 
 app = tk.Tk()
-app.title("Place() method")
-app.geometry('200x200')
+app.title("PROGRAMAS BAR")
+app.geometry('500x400')
 app.resizable(1, 1)
 
 backFrame = tk.Frame(master=app, width=200, height=200, bg = 'yellow').pack()
